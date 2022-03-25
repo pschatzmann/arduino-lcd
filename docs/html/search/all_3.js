d64b1a37260c9957150ba7ebea9cf0a1842f47f8
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-03-25 01:43:21 +0100

    I2C support

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -1,9 +1,10 @@
 var searchData=
 [
   ['lcd_0',['LCD',['../class_l_c_d.html',1,'']]],
-  ['lcd_5fi2c_1',['LCD_I2C',['../class_l_c_d___i2_c.html',1,'']]],
-  ['lcdbargraph_2',['LCDBarGraph',['../class_l_c_d_bar_graph.html',1,'LCDBarGraph'],['../class_l_c_d_bar_graph.html#ac1f1cad334f0148dfbdb6f1f1a25736a',1,'LCDBarGraph::LCDBarGraph()']]],
-  ['lcdclient_3',['LCDClient',['../class_l_c_d_client.html',1,'']]],
-  ['lcddriver_4',['LCDDriver',['../struct_l_c_d_driver.html',1,'']]],
-  ['lcdwritedriver_5',['LCDWriteDriver',['../struct_l_c_d_write_driver.html',1,'']]]
+  ['lcd_20library_1',['LCD Library',['../index.html',1,'']]],
+  ['lcd_5fi2c_2',['LCD_I2C',['../class_l_c_d___i2_c.html',1,'']]],
+  ['lcdbargraph_3',['LCDBarGraph',['../class_l_c_d_bar_graph.html',1,'LCDBarGraph'],['../class_l_c_d_bar_graph.html#ac1f1cad334f0148dfbdb6f1f1a25736a',1,'LCDBarGraph::LCDBarGraph()']]],
+  ['lcdclient_4',['LCDClient',['../class_l_c_d_client.html',1,'']]],
+  ['lcddriver_5',['LCDDriver',['../struct_l_c_d_driver.html',1,'']]],
+  ['lcdwritedriver_6',['LCDWriteDriver',['../struct_l_c_d_write_driver.html',1,'']]]
 ];
---
+++ docs/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['lcd_20library_0',['LCD Library',['../index.html',1,'']]]
+];
